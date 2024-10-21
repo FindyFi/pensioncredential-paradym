a309acf7e9111a053a2f6d40f7627ea2f6d45ca2
Samuel Rinnetmäki <samuel@iki.fi>
2024-10-21 10:40:28 +0300

    added rudimentary support for OID Federation

--- verifier.js
+++ verifier.js
@@ -212,6 +212,11 @@ const handleRequests = async function (req, res) {
   let id = fullUrl.searchParams.get('id')
   console.log(fullUrl.pathname, id)
   switch (fullUrl.pathname) {
+    case '/.well-known/openid-federation':
+      res.setHeader("Content-Type", "application/entity-statement+jwt")
+      res.writeHead(200)
+      res.end(config.verifier_entity_configuration)
+      return false
     case '/status':
       const status = await getStatus(id)
       res.setHeader("Content-Type", "application/json")
